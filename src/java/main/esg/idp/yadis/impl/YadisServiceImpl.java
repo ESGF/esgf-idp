b5c9722b3213592356cbc5f2a360e962812693b5
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2018-10-25 10:39:05 -0600

    Upgrading dom4j and spring

--- src/java/main/esg/idp/yadis/impl/YadisServiceImpl.java
+++ src/java/main/esg/idp/yadis/impl/YadisServiceImpl.java
@@ -27,11 +27,11 @@ import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
-import org.jdom.Document;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
